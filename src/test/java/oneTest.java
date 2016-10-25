f70920ac40fe725c9d270c0873f6e72ff3e25c4c
blinkday <blinkday@163.com>
2016-10-25 17:39:10 +0800

    add new project

---
+++ src/test/java/oneTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static junit.framework.Assert.*;
+
+/**
+ * Created by zyj on 16/10/22.
+ */
+public class oneTest {
+    @Test
+    public void test1(){
+        System.out.println("yes!");
+        assertEquals("1","1");
+    }
+
+}
---
+++ src/test/java/twoTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static junit.framework.Assert.*;
+/**
+ * Created by zyj on 16/10/22.
+ */
+public class twoTest {
+    @Test
+    public void test2(){
+        System.out.println("no!");
+    }
+}
